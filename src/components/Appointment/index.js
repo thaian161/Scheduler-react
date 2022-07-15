665b6a088c8717bd2083767ad6d99d5382f79fc0
thaian161 <thaian161@yahoo.com>
2022-07-14 21:50:34 -0500

    completed Displaying Appointments: Appointment Empty and Appointment Booked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import 'components/Appointment/styles.scss';
 
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview && (
+        <Show
+          interviewer={props.interview.interviewer}
+          student={props.interview.student}
+        />
+      )}
+      {!props.interview && <Empty />}
+    </article>
+  );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
@@ -206,4 +206,24 @@ storiesOf('Appointment', module)
       onSave={action('onSave')}
       onCancel={action('onCancel')}
     />
+  ))
+
+  //APOINTMENT EMPTY
+  .add('Appointment Empty', () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+
+  //APPOINTMENT BOOKED
+  .add('Appointment Booked', () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: 'Lydia Miller-Jones', interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
   ));
