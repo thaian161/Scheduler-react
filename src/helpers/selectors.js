b5aba257d5d33c34822e7a93d0da5956e829c079
thaian161 <thaian161@yahoo.com>
2022-07-16 17:36:47 -0500

    wrote getAppointmentsForDay helper function, added test using Jest

---
+++ src/helpers/selectors.js
@@ -0,0 +1,23 @@
+//Helper Func
+
+//helps return an array of appointments for a specfic day
+export function getAppointmentsForDay(state, day) {
+  const emptyArray = [];
+
+  //Check to see if "day" is available in the State Obj
+  const checkIfDayMatchWithNameInDaysObj = state.days.find(
+    (nameOfDays) => nameOfDays.name === day
+  );
+
+  //if day is available/ truthy then map it to get the Array of Appointments using appointments.id
+  if (checkIfDayMatchWithNameInDaysObj) {
+    const appointmentsArray = checkIfDayMatchWithNameInDaysObj.appointments.map(
+      (id) => state.appointments[id]
+    );
+
+    return appointmentsArray;
+  }
+  if (checkIfDayMatchWithNameInDaysObj === undefined) {
+    return emptyArray;
+  }
+}
