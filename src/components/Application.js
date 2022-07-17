4acf096a1c0edb734ffba6c2b01e7a2ec14091b5
thaian161 <thaian161@yahoo.com>
2022-07-16 21:04:04 -0500

    combined all states into 1 state obj

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,50 +6,26 @@ import DayList from './DayList';
 import 'components/Appointment';
 import Appointment from 'components/Appointment';
 
-//MOCK DATA
-const appointments = {
-  1: {
-    id: 1,
-    time: '12pm',
-  },
-  2: {
-    id: 2,
-    time: '1pm',
-    interview: {
-      student: 'Lydia Miller-Jones',
-      interviewer: {
-        id: 3,
-        name: 'Sylvia Palmer',
-        avatar: 'https://i.imgur.com/LpaY82x.png',
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: '2pm',
-  },
-  4: {
-    id: 4,
-    time: '3pm',
-    interview: {
-      student: 'Archie Andrews',
-      interviewer: {
-        id: 4,
-        name: 'Cohana Roy',
-        avatar: 'https://i.imgur.com/FK8V841.jpg',
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: '4pm',
-  },
-};
-
 export default function Application(props) {
   //default, first rendering
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState('Monday');
+  // const [days, setDays] = useState([]);
 
+  //combine the state for day, days, and appointments into a state into a single object
+  const [appointments, setAppointments] = useState({});
+
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {},
+  });
+
+  const setDay = (day) => setState({ ...state, day });
+
+  const setDays = (days) => {
+    setState((prev) => ({ ...prev, days }));
+  };
   //FETCH Days API using
   useEffect(() => {
     const dayURL = '/api/days';
@@ -80,7 +56,7 @@ export default function Application(props) {
             days={days}
             day={'Monday'}
             setDay={(day) => console.log(day)} */}
-          <DayList days={days} value={days} onChange={setDays} />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
