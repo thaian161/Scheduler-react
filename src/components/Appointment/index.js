eb3931152e5a1ea7f55520ae88215f7e299fb75a
thaian161 <thaian161@yahoo.com>
2022-07-19 00:55:21 -0500

    handling errors with ERROR_SAVE and ERROR_DELETE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import Empty from './Empty';
 import Form from './Form';
 import Status from './Status';
 import Confirm from './Confirm';
+import Error from './Error';
 
 import useVisualMode from '../../hooks/useVisualMode';
 
@@ -18,6 +19,8 @@ export default function Appointment(props) {
   const DELETE = 'DELETE';
   const CONFIRM = 'CONFIRM';
   const EDIT = 'EDIT';
+  const ERROR_SAVE = 'ERROR_SAVE';
+  const ERROR_DELETE = 'ERROR_DELETE';
 
   //SAVE
   function save(name, interviewer) {
@@ -26,7 +29,10 @@ export default function Appointment(props) {
       interviewer,
     };
     transition(SAVING);
-    props.bookInterview(props.id, interview).then(() => transition(SHOW));
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch((error) => transition(ERROR_SAVE, true));
   }
 
   //DELETE
@@ -38,7 +44,10 @@ export default function Appointment(props) {
 
     transition(DELETE);
 
-    props.cancelInterview(props.id, interview).then(() => transition(EMPTY));
+    props
+      .cancelInterview(props.id, interview)
+      .then(() => transition(EMPTY))
+      .catch((error) => transition(ERROR_DELETE, true));
   }
 
   const { mode, transition, back } = useVisualMode(
@@ -49,6 +58,7 @@ export default function Appointment(props) {
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+
       {mode === SHOW && (
         <Show
           student={props.interview.student}
@@ -57,12 +67,21 @@ export default function Appointment(props) {
           onEdit={() => transition(EDIT)}
         />
       )}
+
       {mode === CREATE && (
         <Form interviewers={props.interviewers} onCancel={back} onSave={save} />
       )}
 
       {mode === SAVING && <Status message={SAVING} />}
+      {mode === ERROR_SAVE && (
+        <Error message="Cannot be saved" onClose={back} />
+      )}
+
       {mode === DELETE && <Status message={DELETE} />}
+      {mode === ERROR_DELETE && (
+        <Error message="Cannot be deleted" onClose={back} />
+      )}
+
       {mode === CONFIRM && (
         <Confirm onConfirm={deleteInterview} onCancel={back} message={DELETE} />
       )}
