7ec93b0df2bdc992814cdfdd5a48a5735310a044
thaian161 <thaian161@yahoo.com>
2022-07-20 15:56:00 -0500

    added mock axios fake data and added more test to application test file

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,22 +1,36 @@
 /*
   We are rendering `<Application />` down below, so we need React.createElement
 */
-import React from "react";
+import React from 'react';
 
 /*
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
 */
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react';
 
 /*
   We import the component that we are testing
 */
-import Application from "components/Application";
+import Application from 'components/Application';
+
+import { waitForElement, fireEvent } from '@testing-library/react';
 
 /*
   A test that renders a React Component
 */
-it("renders without crashing", () => {
-  render(<Application />);
+
+describe('Application', () => {
+  it('renders without crashing', () => {
+    render(<Application />);
+  });
+
+  it('defaults to Monday and changes the schedule when a new day is selected', () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText('Monday')).then(() => {
+      fireEvent.click(getByText('Tuesday'));
+      expect(getByText('Leopold Silvers')).toBeInTheDocument();
+    });
+  });
 });
